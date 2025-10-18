9ca9683fff7dfd0df317f266ab6c840fe2dd4f83
m.v.voronin <m.v.voronin@tinkoff.ru>
2025-10-18 15:04:42 +0300

    update checkstyle.xml and build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -11,6 +11,12 @@ plugins {
 group = "ru.tbank.education"
 version = "1.0-SNAPSHOT"
 
+java {
+    toolchain {
+        languageVersion.set(JavaLanguageVersion.of(25))
+    }
+}
+
 repositories {
     mavenCentral()
 }
